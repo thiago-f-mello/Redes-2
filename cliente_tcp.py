37648bcc19f6f90f34199ff4ea70eefea7191b38
Rafael Urbanek Laurentino <rafaurbanek@rafael_urbanek@hotmail.com>
2025-11-28 03:38:28 -0300

    função upload e list cliente/servidor implementadas

--- cliente_tcp.py
+++ cliente_tcp.py
@@ -31,7 +31,7 @@ if len(sys.argv) != 4:
 
 clientId = int(sys.argv[1])     # Identificador do cliente
 host = sys.argv[2]              # Endereço do servidor
-porta = int(sys.argv[3])        # Porta do servidor
+port = int(sys.argv[3])         # Porta do servidor
 
 def help():
     print ("---------------------------------------- Comandos ----------------------------------------")
@@ -40,72 +40,81 @@ def help():
     print ("exit - encerra a execução do programa")
     print ("help - exibe modo de uso e comandos disponíveis\n")
 
-def initConnection(id, host, porta):
+# Realiza upload de um arquivo para o servidor
+def upload(port, id, arq):
+      
       # Cria socket TCP (IPv4 + TCP)
       sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
       # Conecta ao servidor
-      sockfd.connect((host, porta))
+      sockfd.connect((host, port))
 
-      return sockfd
+      # Retorna caso não encontre o arquivo
+      if not os.path.exists(arq):
+            print(f"[CLIENTE] Arquivo '{arq}' não encontrado.")
+            return
 
-# Realiza upload de arquivo para o servidor
-def upload(skt, id, host, porta, arq):
       # Pega informações do arquivo
-      info = os.stat(arq)
-      buffer = f"{id}\n{arq}\n{info.st_size}\n{info.st_mtime}\n{info.st_ctime}\n"
+      metaData = os.stat(arq)
+      header = (
+           f"{id}\n"
+           f"upload\n"
+           f"{arq}\n"
+           f"{metaData.st_size}\n"
+           f"{metaData.st_mtime}\n"
+           f"{metaData.st_atime}\n"
+           f"{metaData.st_mode}\n"
+           "\n"
+      )
 
       # Envia o "header" textual para o servidor
-      skt.sendall(buffer.encode())
+      sockfd.sendall(header.encode())
 
       # Lê e envia bytes do arquivo
       with open(arq, "rb") as f:
-            while True:
-                  # Lê próximo bloco do arquivo
-                  buffer = f.read(TAM_MAX)
-
-                  # Se não há mais dados, encerra o envio
-                  if not buffer:
-                        break
-                  
-                  # Envia bloco lido
-                  skt.sendall(buffer)
+            while chunk := f.read(TAM_MAX):
+                  sockfd.sendall(chunk)
 
       print("[CLIENTE] Arquivo enviado.")
 
       # Recebe resposta do servidor
-      resposta = skt.recv(TAM_MAX).decode()
-
+      resposta = sockfd.recv(TAM_MAX).decode()
       print(f"[CLIENTE] Resposta recebida: {resposta}")
+
+      sockfd.close()
      
-     
+def list_files(port, id):    
+      # Cria socket TCP (IPv4 + TCP)
+      sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def list(skt,id, host, porta):
-     # Conecta ao servidor
-     skt.connect((host, porta))
+      # Conecta ao servidor
+      sockfd.connect((host, port))
+      
+      # header inicial
+      header = (
+           f"{id}\n"
+           f"list\n"
+           "\n"
+      )
 
-     # Lê mensagem do usuário
-     buffer = "listar"
+      # Envia mensagem para o servidor
+      sockfd.send(header.encode())
 
-     # Envia mensagem para o servidor
-     skt.send(buffer.encode())
+      # Recebe resposta do servidor
+      content = sockfd.recv(TAM_MAX).decode()
+      print(f"{content}")
 
-     # Recebe resposta do servidor
-     resposta = skt.recv(TAM_MAX).decode()
+      sockfd.close()
 
-     print(f"[CLIENTE] Resposta recebida: {resposta}")
 
-def exit(skt):
-     print()
-     # Fecha socket
-     skt.close()
-
-skt = initConnection(clientId, host, porta)
-
-while(True):
+while True:
 
     print ("Para listar todos os comandos digite 'help'")
     userInput = input("Digite um comando: ")
+    
+    if not userInput.strip():
+        continue
+    
     inputList = userInput.split()
     op = inputList[0]
         
@@ -115,15 +124,18 @@ while(True):
                 print ("Só é possível fazer upload de 1 arquivo por vez; Uso correto: upload <arquivo>\n")
                 continue
               print (f"Fazendo upload do arquivo {inputList[1]}\n")
-              upload(skt, clientId, host, porta, inputList[1])
+              upload(port, clientId, inputList[1])
+        
         case _ if op == options[1]:
               print("Listando todos arquivos do servidor\n")
-              list(skt, clientId, host, porta)
+              list_files(port, clientId)
+       
         case _ if op == options[2]:
-              print ("Encerrando execução\n")
-              exit(skt)
+              print ("Encerrando execução...")
               break
+        
         case _ if op == options[3]:
               help()
+        
         case _:
-              print ("Comando não encontrado\n")
+              print ("Comando desconhecido\n")
--- servidor_tcp.py
+++ servidor_tcp.py
@@ -17,71 +17,166 @@
 #     python3 servidor_tcp.py 8500
 #
 # ***************************************************************
-
+import os
 import socket
 import sys
+import json
 
-TAM_MAX = 1024   # Tamanho máximo do buffer de dados
-TAM_FILA = 5     # Tamanho da fila de conexões
+TAM_MAX = 1024          # Tamanho máximo do buffer de dados
+TAM_FILA = 5            # Tamanho da fila de conexões/Clientes
+DEBUG = False           # Flag para as informações de DEBUG
+TAM_REP = 3             # Quantidade de Réplicas
+DIRNAME = "Server"      # Nome do diretório do servidor
+DEFAULT_PORT = 9000     # Porta padrão para as réplicas
+ACK = "ok"              # ACK para confirmação de recebimento
 
-# Verifica uso correto
-if len(sys.argv) != 2:
-    print(f"Uso correto: {sys.argv[0]} <porta>")
-    sys.exit(1)
-
-porta = int(sys.argv[1])  # Porta do servidor, converte para int
-
-# Cria socket TCP (IPv4 + TCP)
-sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sockfd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# Associa o socket a todas as interfaces locais
-sockfd.bind(('', porta))
-
-# Coloca o socket em modo de escuta
-sockfd.listen(TAM_FILA)
-
-print("[SERVIDOR] Aguardando conexões...")
-
-# Loop principal: aceita conexões iterativamente
-while True:
-    connfd, cliente_addr = sockfd.accept()
-    print(f"[SERVIDOR] Cliente conectado: {cliente_addr}")
-
-    # Recebe as informações do arquivo
-    header = connfd.recv(TAM_MAX).decode()
+# Função para criar os sockets para se conectar com as N réplicas
+def createReplicasSockets(port):
     
-    # Processa o "header" recebido
-    info = header.split("\n")
-    clientId = info[0]
-    nome = info[1]
-    tamanho = int(info[2])
-    mtime = info[3]
-    ctime = info[4]
+    replicasPorts = [DEFAULT_PORT + i for i in range(1, TAM_REP + 1)]
+    replicasSockets = []
+
+    # Cria os sockets
+    for port in replicasPorts:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("", port))
+        replicasSockets.append(sock)
+
+# Função para criar o socket para se conectar com o cliente
+def createClientSocket(port):
+    
+    # Cria socket TCP (IPv4 + TCP)
+    sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sockfd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Associa o socket a todas as interfaces locais
+    sockfd.bind(('', port))
+
+    return sockfd
+
+# Função para criar o diretório do cliente no servidor local
+def createClientDirectory(id):
+    clientPath = ("./" + DIRNAME + "/" + id + "/")
+
+    if not os.path.isdir(clientPath):
+        try:
+            os.mkdir(clientPath)
+            if DEBUG: print(f"Diretório '{clientPath}' criado com sucesso!")
+        except Exception as ex:
+            if DEBUG: print("Erro: ", ex)
+    
+    return clientPath
+
+# Função que faz o upload do arquivo e chama a função de replicação
+def uploadFile(skt, header):
+    clientId = header[0]
+    arqName = header[2]
+    arqSize = int(header[3])
+    arqmtime = header[4]
+    arqatime = header[5]
+    arqMode = header[6]
+    
+    # Prepara o caminho do arquivo
+    arqPath = (createClientDirectory(clientId) + arqName)
     
     # Recebe o arquivo em si
-    with open(nome, "wb") as f:
-        bytes_lidos = 0
+    with open(arqPath, "wb") as f:
+        readBytes = 0
 
         # Recebe o arquivo em partes
-        while bytes_lidos < tamanho:
+        while readBytes < arqSize:
             # Recebe próximo bloco do arquivo
-            buffer = connfd.recv(TAM_MAX)
-
+            buffer = skt.recv(TAM_MAX)
             # Se não há mais dados, encerra o recebimento
             if not buffer:
                 break
-
             # Escreve os bytes recebidos no arquivo
             f.write(buffer)
-
             # Atualiza a quantidade total de bytes lidos
-            bytes_lidos += len(buffer)
+            readBytes += len(buffer)
+    
+    # Aplica permissões
+    os.chmod(arqPath, int(arqMode))
+    # Aplica timestamps (acesso e modificação)
+    os.utime(arqPath, (float(arqatime), float(arqmtime)))
 
-    buffer = "Recebido com sucesso!"
-    # Envia a mesma mensagem (codificada em bytes)
-    connfd.send(buffer.encode())
+    print(f"Arquivo '{arqName}' recebido. Armazenamento local concluído.")
+    
+    # Envia a mesma mensagem de confirmação
+    print("Replicação concluída com sucesso. Enviando confirmação ao cliente.")
+    skt.send(ACK.encode())
 
     # Fecha a conexão com o cliente
-    connfd.close()
-    print("[SERVIDOR] Conexão encerrada.\n")
+    skt.close()
+    if DEBUG: print("[SERVIDOR] Conexão encerrada.\n")
+
+# Função para listar os arquivos do cliente
+def listFiles(skt, id):
+    
+    # Prepara o caminho do diretório do cliente
+    arqPath = createClientDirectory(id)
+    content = os.listdir(arqPath)
+
+    skt.send(str(content).encode())
+    
+    # Fecha a conexão com o cliente
+    skt.close()
+    if DEBUG: print("[SERVIDOR] Conexão encerrada.\n")
+
+
+
+# -------------------------------------- Início do Main --------------------------------------
+
+# Verifica uso correto, com passagem para debug
+# Sem argumentos suficientes
+if len(sys.argv) < 2:
+    print(f"Uso correto: {sys.argv[0]} <porta 'menor que 9000'>")
+    sys.exit(1)
+# Verifica se é modo debug
+if len(sys.argv) == 3 and sys.argv[2].lower() == "debug":
+    DEBUG = True
+# Porta do servidor/Cliente, converte para int
+clientPort = int(sys.argv[1])
+# Valida a porta
+if clientPort >= 9000:
+    print(f"Erro: porta deve ser menor que 9000 (recebido {clientPort}).")
+    sys.exit(1)
+
+# Cria o diretório do servidor principal se não existir
+if not os.path.isdir(DIRNAME):
+    try:
+        os.mkdir(DIRNAME)
+        if DEBUG: print(f"Diretório '{DIRNAME}' criado com sucesso!")
+    except Exception as ex:
+        if DEBUG: print("Erro: ", ex) 
+
+# Cria o socket para se comunicar com o cliente
+clientSocket = createClientSocket(clientPort)
+# Coloca o socket em modo de escuta
+clientSocket.listen(TAM_FILA)
+if DEBUG: print("[SERVIDOR] Aguardando conexões...")
+
+# Loop principal: aceita conexões iterativamente
+while True:
+    # Aceita a conexão
+    auxSocket, client_addr = clientSocket.accept()
+    # Recebe as informações do arquivo (IMPLEMENTAR PARA HEADERS QUE SEJAM MAIORES QUE O TAM_MAX)
+    header = (auxSocket.recv(TAM_MAX).decode()).split("\n")
+
+    # Processa o "header" recebido
+    clientId = header[0]
+    operation = header[1]
+
+    print(f"[SERVIDOR] Conexão com cliente [{clientId}] estabelecida")
+    if DEBUG: print(f"Endereço: {client_addr}")
+    print(f"Operação solicitada: {operation}")
+
+    match operation.lower():
+        case "upload":
+              uploadFile(auxSocket, header)
+        
+        case "list":
+              listFiles(auxSocket, clientId)
+
+        case _:
+              print ("Comando desconhecido\n")
